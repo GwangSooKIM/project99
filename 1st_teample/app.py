8467f5758567432ef16022467a78be8106978168
hyeonginju <guddls64@naver.com>
2021-03-02 12:30:17 +0900

    asdasd

--- 1st_teample/app.py
+++ 1st_teample/app.py
@@ -96,6 +96,9 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
+
+
+"""
 @app.route('/update_profile', methods=['POST'])
 def save_img():
     token_receive = request.cookies.get('mytoken')
@@ -129,6 +132,7 @@ def get_posts():
         return redirect(url_for("home"))
 
 
+
 @app.route('/update_like', methods=['POST'])
 def update_like():
     token_receive = request.cookies.get('mytoken')
@@ -138,7 +142,7 @@ def update_like():
         return jsonify({"result": "success", 'msg': 'updated'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
+"""
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
